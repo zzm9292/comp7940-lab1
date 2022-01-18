e69f99354dfec220c5a59685d56f59e44e5fa46c
zzm9292 <zzm9292@qq.com>
2022-01-18 21:24:18 +0800

    Ex1 commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def main():
-    print("Hello World")
-
-if __name__ == '__main__':
-    main()
+# Find the all factors of x using a loop and the operator % 
+# % means find remainder, for example 10 % 2 = 0; 10% 3 = 1
+x = 52633
+for i in range(x+1):
+  # your code here
